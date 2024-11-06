cb0de945e16533114e3bb93c8282eb087f93252a
matias-araya-02 <matiasaraya177@gmail.com>
2024-11-06 14:47:33 -0300

    MiniSaborcito!

---
+++ client/js/products.js
@@ -0,0 +1,23 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Hamburguesa", 
+        price: 30,
+        quanty: 1,
+        img: "./media/hamburguesa.png",
+    },
+    {
+        id: 2,
+        productName: "Papas Fritas", 
+        price: 20,
+        quanty: 1,
+        img: "./media/papasFritas.png",
+    },
+    {
+        id: 3,
+        productName: "Coca Cola", 
+        price: 10,
+        quanty: 1,
+        img: "./media/cocaCola.png",
+    },
+];
